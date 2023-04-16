c7ba5fe1eacbd13bf9201c716dfb8114c5bede8a
Ugnius Terekas <u.terekas@vienasaskaita.lt>
2023-04-16 19:12:50 +0300

    User Entertainment Reservation migrations added

---
+++ API/DataLayer/Migrations/20230416160802_UserRelationsEntertainmentReservation.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRelationsEntertainmentReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Entertainments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entertainments_UserId",
+                table: "Entertainments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entertainments_Users_UserId",
+                table: "Entertainments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entertainments_Users_UserId",
+                table: "Entertainments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Entertainments_UserId",
+                table: "Entertainments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Entertainments");
+        }
+    }
+}
